cdc97d5051e739c680a6757de2b44570c4a7940b
Mattias Benngard <mbengan@gmail.com>
2016-02-10 18:57:40 +0100

    Add SQL Stump
    * databaseEntities; CREATE TABLE - done and verified
    * databaseRelations; CREATE TABLE - started and verified.
    * Moved Database\docs\ to docs\

---
+++ db/databaseEntities.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE Activity
+(
+	Name			varchar(255) 	NOT NULL,
+	Description		text,
+	Points 			int				DEFAULT 0,
+	Time			int,
+	PRIMARY KEY (Name)
+);
+
+CREATE TABLE Mood
+(
+	Type			varchar(255)	NOT NULL,
+	PRIMARY KEY (Type)
+);
+
+CREATE TABLE Trophy
+(
+	Name			varchar(255)	NOT NULL,
+	Description		text,
+	Quality			varchar(6)		NOT NULL,
+	PRIMARY KEY (Name),
+	CHECK (Quality='Bronze' OR Quality='Silver' OR Quality='Gold')
+);
+
+CREATE TABLE User
+(
+	Name			varchar(255)	NOT NULL,
+	PRIMARY KEY (Name)
+);
+
+CREATE TABLE Challenge
+(
+	Name			varchar(255)	NOT NULL,
+	Description		text,
+	StartTime		int,
+	EndTime			int,
+	PRIMARY KEY (Name)
+);
---
+++ db/databaseRelations.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE GoodFor
+(
+	moodType			varchar(255)	NOT NULL,
+	activityName		varchar(255)	NOT NULL,
+	PRIMARY KEY (moodType, activityName),
+	FOREIGN KEY (moodType) REFERENCES Mood(type),
+	FOREIGN KEY (activityName) REFERENCES Activity(name)
+);
+
+CREATE TABLE ActivityCompleted
+(
+	userName			varchar(255)	NOT NULL,
+	activityName		varchar(255)	NOT NULL,
+	PRIMARY KEY (userName, activityName),
+	FOREIGN KEY (userName) REFERENCES User(name),
+	FOREIGN KEY (activityName) REFERENCES Activity(name)
+);
+
+CREATE TABLE OnGoingActivity
+(
+	userName			varchar(255)	NOT NULL,
+	activityName		varchar(255)	NOT NULL,
+	PRIMARY KEY (userName, activityName),
+	FOREIGN KEY (userName) REFERENCES User(name),
+	FOREIGN KEY (activityName) REFERENCES Activity(name)
+);
