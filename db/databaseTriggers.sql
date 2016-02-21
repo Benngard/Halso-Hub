e12befb5d3aa779b2350f4bac1e6f791cb27279a
Edsinger <danieledsinger@hotmail.com>
2016-02-21 16:26:28 +0100

    New view and trigger
    * Add view ChallengeActivitiesLeftInCurrentChallenge
    * Add update and insert trigger for ActivityCompleted

--- db/databaseTriggers.sql
+++ db/databaseTriggers.sql
@@ -1,9 +1,56 @@
-CREATE TRIGGER `halso_hub`.CompleteActivity
+DELIMITER $$
+
+
+/*	 ActivityCompleted	 */
+
+CREATE TRIGGER `halso_hub`.AddCompleteActivity
 BEFORE INSERT ON `halso_hub`.ActivityCompleted
 
 FOR EACH ROW
-BEGIN
 
+BEGIN
+	DECLARE activityNr integer;
+    DECLARE activityNrToday integer;
+    
+	SELECT IFNULL(ActivityCompleted.nrAllTime, 0) INTO activityNr
+    FROM ActivityCompleted
+	WHERE NEW.userName = ActivityCompleted.userName AND NEW.ActivityName = ActivityCompleted.activityName;
+    
+    SELECT IFNULL(ActivityCompleted.nrToday, 0) INTO activityNrToday
+    FROM ActivityCompleted
+	WHERE NEW.userName = ActivityCompleted.userName AND NEW.ActivityName = ActivityCompleted.activityName;
+    
+    
+    IF (activityNr > 0) THEN
+		UPDATE ActivityCompleted SET ActivityCompleted.nr = (activityNr+1)
+			WHERE ActivityCompleted.userName = NEW.userName AND ActivityCompleted.activityName = NEW.activityName;
+		CALL raise_application_error(-20001, 'Already have a counter for activity');
 	
+    ELSE 
+		INSERT INTO ActivityCompleted
+			VALUES(NEW.userName, NEW.activityName. activityNr+1, activityNr);
+	END IF;
+
 
 END;
+$$
+
+
+CREATE TRIGGER `halso_hub`.UpdateCompleteActivity
+BEFORE UPDATE ON `halso_hub`.ActivityCompleted
+
+FOR EACH ROW
+
+BEGIN
+	DECLARE activityNr integer;
+    
+	SELECT IFNULL(ActivityCompleted.nr, 0) INTO activityNr
+    FROM ActivityCompleted
+	WHERE NEW.userName = ActivityCompleted.userName AND NEW.ActivityName = ActivityCompleted.activityName;
+    
+	SET NEW.nr = activityNr+1;
+
+END; 
+$$
+
+DELIMITER ;
--- db/databaseViews.sql
+++ db/databaseViews.sql
@@ -1,61 +1,70 @@
 
 CREATE VIEW `halso_hub`.UserTotalPoints AS
 (
-	SELECT User.Name, IFNULL( SUM(activity.Points), 0) AS TotalPoints
+	SELECT Users.Name, IFNULL( SUM(activity.Points), 0) AS TotalPoints
 	FROM activity LEFT JOIN ActivityCompleted
 		ON ActivityCompleted.activityName = activity.Name
-    RIGHT JOIN user
-		ON ActivityCompleted.userName = user.Name
-    GROUP BY User.Name
+    RIGHT JOIN Users
+		ON ActivityCompleted.userName = Users.Name
+    GROUP BY Users.Name
     
 );
 
 CREATE VIEW `halso_hub`.UserTotalTrophies AS
 (
-	SELECT User.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
+	SELECT Users.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
 	FROM Trophy LEFT JOIN EarnedTrophy
 		ON EarnedTrophy.trophyName = Trophy.Name
-    RIGHT JOIN user
-		ON EarnedTrophy.userName = user.Name
-    GROUP BY User.Name
+    RIGHT JOIN Users
+		ON EarnedTrophy.userName = Users.Name
+    GROUP BY Users.Name
 );
 
 CREATE VIEW `halso_hub`.UserTotalBronze AS
 (
-	SELECT User.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
+	SELECT Users.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
 	FROM Trophy LEFT JOIN EarnedTrophy
 		ON EarnedTrophy.trophyName = Trophy.Name AND Trophy.Quality = 'Bronze'
-    RIGHT JOIN user
-		ON EarnedTrophy.userName = user.Name
-    GROUP BY User.Name
+    RIGHT JOIN Users
+		ON EarnedTrophy.userName = Users.Name
+    GROUP BY Users.Name
     
 );
 
 CREATE VIEW `halso_hub`.UserTotalSilver AS
 (
-	SELECT User.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
+	SELECT Users.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
 	FROM Trophy LEFT JOIN EarnedTrophy
 		ON EarnedTrophy.trophyName = Trophy.Name AND Trophy.Quality = 'Silver'
-    RIGHT JOIN user
-		ON EarnedTrophy.userName = user.Name
-    GROUP BY User.Name
+    RIGHT JOIN Users
+		ON EarnedTrophy.userName = Users.Name
+    GROUP BY Users.Name
     
 );
 
 CREATE VIEW `halso_hub`.UserTotalGold AS
 (
-	SELECT User.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
+	SELECT Users.Name, IFNULL( COUNT(Trophy.Name), 0) AS TotalPoints
 	FROM Trophy LEFT JOIN EarnedTrophy
 		ON EarnedTrophy.trophyName = Trophy.Name AND Trophy.Quality = 'Gold'
-    RIGHT JOIN user
-		ON EarnedTrophy.userName = user.Name
-    GROUP BY User.Name
+    RIGHT JOIN Users
+		ON EarnedTrophy.userName = Users.Name
+    GROUP BY Users.Name
     
 );
 
+
 CREATE VIEW `halso_hub`.ChallengeActivitiesLeftInCurrentChallenge AS
 (
-	SELECT 
+	SELECT OnGoingChallenge.userName, ChallengeRequires.activityName, ChallengeRequires.nr, ChallengeRequires.nr-ActivityCompleted.nrToday AS nrLeft
+    FROM OnGoingChallenge JOIN ChallengeRequires
+		ON OnGoingChallenge.challengeName = ChallengeRequires.challengeName
+	LEFT JOIN ActivityCompleted
+		ON ActivityCompleted.activityName = ChallengeRequires.activityName AND ActivityCompleted.nrToday < ChallengeRequires.nr
+	
+	ORDER BY OnGoingChallenge.userName
 	
 	
 );
+
+
